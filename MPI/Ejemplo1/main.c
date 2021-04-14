194736c7c2cda3e105fd9273e6f54d80c8fee85e
Israel0806 <israel0806_@hotmail.com>
2021-04-14 17:25:26 +0000

    mpi example

--- MPI/Ejemplo1/main.c
+++ MPI/Ejemplo1/main.c
@@ -1,17 +1,16 @@
-#include <iostream>
-#include <vector>
+#include <stdio.h>
 #include "mpi.h"
-using namespace std;
 
 int main() {
     int my_rank, comm_sz;
-    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int arr[] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    int arr_size = 12;
 	int local_sum = 0;
     MPI_Init(NULL, NULL);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     if(my_rank != 0){
-        int range = arr.size() / (comm_sz-1);
+        int range = arr_size / (comm_sz-1);
         int range_min = (my_rank - 1) * range;
         int range_max = (my_rank - 1) * range + range;
         for (int i = range_min; i < range_max; ++i)
@@ -24,7 +23,7 @@ int main() {
             MPI_Recv(&local_sum, 1, MPI_INT,i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             total_sum += local_sum;
         }
-		cout<<"The total Sum of the array is:  "<<total_sum;
+		printf("The total Sum of the array is: %i", total_sum);
     }
     MPI_Finalize();
 	return 0;
